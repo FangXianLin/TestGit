eefc7efdc96bce64ab04cb2f7d30c5452bcf1a51
Fxl <2572601783@qq.com>
2022-05-01 15:09:29 +0800

    push commit

--- src/main/java/com/fxl/git/testGit.java
+++ src/main/java/com/fxl/git/testGit.java
@@ -7,5 +7,6 @@ public class testGit {
         System.out.println("hot-fix");
         System.out.println("master git");
         System.out.println("hot-fix2");
+        System.out.println("push test");
     }
 }
